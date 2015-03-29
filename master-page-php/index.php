2474b3e416247b44a3e2646e2688273ea87ef845
rorduno <rorduno88@gmail.com>
2015-03-29 13:49:40 -0500

    uploaded src

---
+++ master-page-php/index.php
@@ -0,0 +1,23 @@
+<?php
+	$page_title = 'Home';//Names page and <h1>
+	$thisPage='index';//Gives the page an indentifier
+	include ('./includes/header.html');
+?>
+<!-- Content -->
+
+<h1><?php echo $page_title ?></h1>
+<p>Bacon ipsum dolor amet pork loin pancetta filet mignon strip
+	steak salami sirloin pork doner. Rump porchetta bresaola, spare
+	ribs ball tip brisket t-bone shankle chicken. Shankle prosciutto
+	chuck, shoulder bacon flank short ribs pork chop kielbasa pig
+	cupim. Tri-tip landjaeger pancetta pork, chuck t-bone biltong ham
+	pork loin. Turkey tail chuck, shank bresaola capicola corned beef
+	short loin. Shoulder picanha kielbasa, andouille leberkas tongue
+	doner jerky ham hock sausage cupim frankfurter rump spare ribs pork
+	belly.
+</p>
+<!-- Content ends -->
+<?php
+	include ('./includes/footer.html');
+?>
+
